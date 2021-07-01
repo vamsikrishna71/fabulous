279810e04181b1c8d3fafb1e9114d2ea5645bc86
vamsikrishna71 <31164677+vamsikrishna71@users.noreply.github.com>
2021-07-01 09:11:28 +0530

    Up to services

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // SlideShow
+    $('#banner-area .owl-carousel').owlCarousel({
+        dots: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 1000,
+        items: 1,
+    });
+});
